77e90fd54aefd534c37e598c4fefc306cfa134b8
Dave Tyrrell <dgtyrrell@gmail.com>
2014-08-09 16:42:57 +1200

    SHA and Scrypt
    Added SHA and Scrypt network information

---
+++ p2pool/networks/myriad_sha256d.py
@@ -0,0 +1,17 @@
+from p2pool.bitcoin import networks
+
+PARENT = networks.nets['myriad_sha256d']
+SHARE_PERIOD = 10 # seconds
+CHAIN_LENGTH = 24*60*60//10 # shares
+REAL_CHAIN_LENGTH = 24*60*60//10 # shares
+TARGET_LOOKBEHIND = 50 # shares
+SPREAD = 30 # blocks
+IDENTIFIER = 'fc70135c700a00ee'.decode('hex')
+PREFIX = '9472ef181e88efcb'.decode('hex')
+P2P_PORT = 5577
+MIN_TARGET = 0
+MAX_TARGET = 2**256//2**20 - 1
+PERSIST = True
+WORKER_PORT = 5578
+BOOTSTRAP_ADDRS = 'birdspool.no-ip.org'.split(' ')
+VERSION_CHECK = lambda v: True
